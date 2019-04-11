2b57580dada0a87e486d70dee1b478ae65eb0cbc
zhangsan <zhangsan@qq.com>
2019-04-11 01:01:41 +0000

    a

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\DB;
 class WeixinController extends Controller
 {
     public function accessToken(){
-        //Cache::pull('access');exit;
+        Cache::pull('access');exit;
         $access = Cache('access');
         if(empty($access)){
             $appid = "wx51db63563c238547";
@@ -33,60 +33,99 @@ class WeixinController extends Controller
         $con = mysqli_connect('127.0.0.1', 'root', '123456', 'test');
         $str = file_get_contents("php://input");
         $objxml = simplexml_load_string($str);
-        var_dump($objxml);
+        //var_dump($objxml);
         file_put_contents("/tmp/1809_weixin.log", $str, FILE_APPEND);
 
-        $MsgType = $objxml->MsgType;
+        $Event = $objxml->Event;
+        $FromUserName = $objxml->FromUserName;
+        $ToUserName = $objxml->ToUserName;
+        $access = $this->accessToken();
+        $url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=$access";
+        $info = file_get_contents($url);
+        $arrInfo = json_decode($info,true);
+        $data = $arrInfo['data'];
+        //var_dump($data);exit;
+        $openid = $data['openid'];
+        foreach($openid as $k=>$v){
+            $userUrl="https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access&openid=$v&lang=zh_CN";
+            $userAccessInfo=file_get_contents($userUrl);
+            $userInfo=json_decode($userAccessInfo,true);
+            $datainfos[]=$userInfo;
+        }
+        var_dump($datainfos);exit;
+
+
+
         //var_dump($MsgType);
-        if($MsgType=='text'){
-            file_put_contents("/tmp/test.log", $str, FILE_APPEND);
+        if($Event='subscribe'){
+            $data=DB::table('wx')->where('openid',$FromUserName)->count();
+            //print_r($data);die;
+            if($data=='0'){
+                $weiInfo=[
+                    'name'=>$name,
+                    'sex'=>$info['sex'],
+                    'img'=>$info['headimgurl'],
+                    'openid'=>$info['openid'],
+                    'time'=>time()
+                ];
+                //print_r($weiInfo);
+                DB::table('wx')->insert($weiInfo);
 
+                //回复消息
+                $time=time();
+                $content="关注本公众号成功";
+                $xmlStr="
+           <xml>
+                <ToUserName><![CDATA[$FromUserName]]></ToUserName>
+                <FromUserName><![CDATA[$ToUserName]]></FromUserName>
+                <CreateTime>$time</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[$content]]></Content>
+           </xml>";
+                echo $xmlStr;
 
-            $content = $objxml->Content;
-            $openid = $objxml->FromUserName;
-            $createtime = $objxml->CreateTime;
-
-            $arr = [
-                'content'=>$content,
-                'openid'=>$openid,
-                'createtime'=>$createtime
-            ];
-
-            $info =DB::table('content')->insert($arr);
+            }else{
+                $time=time();
+                $content="欢迎".$name."回来";
+                $xmlStr="
+           <xml>
+                <ToUserName><![CDATA[$FromUserName]]></ToUserName>
+                <FromUserName><![CDATA[$ToUserName]]></FromUserName>
+                <CreateTime>$time</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[$content]]></Content>
+           </xml>
+       ";
+                echo $xmlStr;
+            }
 
         }
-    }
-
-    /**
-    //获取用户的基本信息
-    public function userInfo(){
-    $access = $this->accessToken();
-    $url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=$access";
-    $info = file_get_contents($url);
-    $arrInfo = json_decode($info,true);
-    $data = $arrInfo['data'];
-    //var_dump($data);exit;
-    $openid = $data['openid'];
-    foreach($openid as $k=>$v){
-    $userUrl="https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access&openid=$v&lang=zh_CN";
-    $userAccessInfo=file_get_contents($userUrl);
-    $userInfo=json_decode($userAccessInfo,true);
-    $datainfos[]=$userInfo;
-    }
-
-    //        $url2="https://api.weixin.qq.com/cgi-bin/tags/get?access_token=$access";
-    //        $info2=file_get_contents($url2);
-    //        $arr2=json_decode($info2,true);
-    //
-    //        $arr = [];
-    //        foreach ($arr2 as $key => $value) {
-    //            $arr = $value;
-    //        }
-    //var_dump($datainfos);exit;
-    return view('user.userlist',['userInfo'=>$datainfos]);
 
     }
-     */
+
+
+   /**
+   //获取用户的基本信息
+   public function userInfo(){
+   $access = $this->accessToken();
+   $url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=$access";
+   $info = file_get_contents($url);
+   $arrInfo = json_decode($info,true);
+   $data = $arrInfo['data'];
+   //var_dump($data);exit;
+   $openid = $data['openid'];
+   foreach($openid as $k=>$v){
+   $userUrl="https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access&openid=$v&lang=zh_CN";
+   $userAccessInfo=file_get_contents($userUrl);
+   $userInfo=json_decode($userAccessInfo,true);
+   $datainfos[]=$userInfo;
+   }
+
+   //return view('user.userlist',['userInfo'=>$datainfos]);
+   return $datainfos;
+   }
+    */
+
 
     /**自定义菜单添加*/
     public function createadd(Request $request){
